1e0fe26308b697225e4ae289d7cd7173fb2cb19e
m-sallam <m-sallam@users.noreply.github.com>
2018-04-08 13:26:40 +0200

    fixes

--- public/js/newItem.js
+++ public/js/newItem.js
@@ -45,7 +45,7 @@ $('#searchBar').on('input', (e) => {
         $('#results').html('<p class="">There is an error with API :/</p>')
         return
       }
-      if (result.result !== undefined) {
+      if (result.result !== []) {
         let html = ''
         searchResults = result.result
         for (let item of result.result) {
